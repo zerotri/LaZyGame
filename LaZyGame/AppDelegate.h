8c91eb967b0fe16a36d144265c9c20d30779450e
Wynter Woods <zerotri@heimdal.useless.lan>
2012-07-08 03:59:38 -0700

    Initial Commit

---
+++ LaZyGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LaZyGame
+//
+//  Created by Wynter Woods on 7/8/12.
+//  Copyright (c) 2012 Wynter Woods. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ LaZyGame/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  LaZyGame
+//
+//  Created by Wynter Woods on 7/8/12.
+//  Copyright (c) 2012 Wynter Woods. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
